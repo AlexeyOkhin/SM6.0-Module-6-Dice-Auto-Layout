cadeb94ccd597930b76c3bcd8774da4ab0912ee8
AlexeyOkhin <k-i-ll-e-r@list.ru>
2023-01-21 14:37:27 +0500

    Initial Commit

---
+++ SM6.0 Module 6 Dice Auto Layout/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SM6.0 Module 6 Dice Auto Layout
+//
+//  Created by Djinsolobzik on 21.01.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
